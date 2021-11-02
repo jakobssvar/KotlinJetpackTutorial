18f29c4fc281d7491568ac1160df5c51332fb55f
svarsstad <sandsvarstad@gmail.com>
2021-11-02 10:51:05 +0100

    tried to change theme but it is not working correctly

--- project/app/src/main/java/com/example/kotlinjetpacktutorial/SplashActivity.kt
+++ project/app/src/main/java/com/example/kotlinjetpacktutorial/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kotlinjetpacktutorial
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.R)    {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }      else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        tvSplash.text = getString(R.string.app_name)
     }
 }
